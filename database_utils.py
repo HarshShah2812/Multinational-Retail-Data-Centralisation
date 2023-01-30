ac32c2525c6cf5df29dbc409b76a1f26b32d4dd4
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-01-30 20:54:46 +0000

    init_db_engine and list_db_tables created

--- database_utils.py
+++ database_utils.py
@@ -1,16 +1,32 @@
 from sqlalchemy import create_engine 
+from sqlalchemy import inspect
 import yaml
 
 class DatabaseConnector:
 
     def __init__(self):
-        self.creds = {}
+        self.read_db_creds()
+        self.init_db_engine()
+        
 
 
     def read_db_creds(self):
        with open ('db_creds.yaml', 'r') as creds:
         creds_loaded = yaml.safe_load(creds)
         print(creds_loaded)
+        return creds_loaded
 
     def init_db_engine(self):
-        engine = create_engine()
+        creds_loaded = self.read_db_creds()
+        engine = create_engine(f"{creds_loaded['DATABASE_TYPE']}+{creds_loaded['DBAPI']}://{creds_loaded['RDS_USER']}:{creds_loaded['RDS_PASSWORD']}@{creds_loaded['RDS_HOST']}:{creds_loaded['RDS_PORT']}/{creds_loaded['RDS_DATABASE']}")
+        engine_connect = engine.connect()
+        return engine_connect
+    
+    def list_db_tables(self, engine):
+        self.init_db_engine()
+        inspector = inspect(engine)
+        list_tables = inspector.get_table_names()
+        return list_tables
+
+if __name__ == '__main__':
+    database_connector = DatabaseConnector()
