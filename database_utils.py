961c5582b3d076aeeb30e1fd1a0dfff34216a516
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-01-30 09:08:11 +0000

    read_db_creds method created

--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,16 @@
+from sqlalchemy import create_engine 
+import yaml
+
 class DatabaseConnector:
+
+    def __init__(self):
+        self.creds = {}
+
+
+    def read_db_creds(self):
+       with open ('db_creds.yaml', 'r') as creds:
+        creds_loaded = yaml.safe_load(creds)
+        print(creds_loaded)
+
+    def init_db_engine(self):
+        engine = create_engine()
