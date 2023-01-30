08f0581eb4d9517147101acfb9f5ff5805408a75
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-01-30 20:56:30 +0000

    read_rds_table method created

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,15 @@
+import pandas as pd
+from database_utils import DatabaseConnector
+
 class DataExtractor:
-    def __init__():
+    
+    def __init__(self):
+        connector = DatabaseConnector()
+
+    def read_rds_table(self, table_name):
+        connector = DatabaseConnector()
+        table_name = connector.list_db_tables()
+        df = pd.read_sql_table(table_name)
+        return df
+
+    
