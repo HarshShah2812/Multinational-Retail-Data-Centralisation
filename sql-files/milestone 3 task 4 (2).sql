806ba472a22912350c74a732160610687a233f15
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-04-09 19:23:41 +0100

    milestone 3 task 4 and milestone 4 task 1 modified

--- sql-files/milestone 3 task 4 (2).sql
+++ sql-files/milestone 3 task 4 (2).sql
@@ -1,3 +1,7 @@
 UPDATE dim_products
-SET weight_class = CASE WHEN weight < 3 THEN 'Light' WHEN weight BETWEEN 3 AND 40 THEN 'Mid_Sized' WHEN weight BETWEEN 41 and 140 THEN 'Heavy' ELSE 'Truck_Required' 
+SET weight_class = 
+CASE WHEN weight < 3 THEN 'Light' 
+WHEN weight BETWEEN 3 AND 40 THEN 'Mid_Sized' 
+WHEN weight BETWEEN 41 and 140 THEN 'Heavy' 
+ELSE 'Truck_Required' 
 END
