2317de93cac243f621600abbf8b92348bc025642
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-04-10 14:30:16 +0100

    milestone 4 task 4 modified, products table removed from query

--- sql-files/milestone 4 task 4.sql
+++ sql-files/milestone 4 task 4.sql
@@ -1,11 +1,9 @@
 SELECT COUNT(*) AS number_of_sales, SUM(o.product_quantity) AS product_quantity_count,
 CASE 
-	WHEN store_type != 'Web Portal' THEN 'Offline'
+	WHEN s.store_type != 'Web Portal' THEN 'Offline'
 	ELSE 'Web'
 END AS location
 FROM orders_table AS o
-LEFT JOIN dim_products AS p
-ON o.product_code = p.product_code
 LEFT JOIN dim_store_details AS s
 ON o.store_code = s.store_code
 GROUP BY location;
