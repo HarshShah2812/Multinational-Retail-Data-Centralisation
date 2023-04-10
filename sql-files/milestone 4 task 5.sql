6072d68b45f65bf90a2ab3fe96d7c05f30e9c276
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-04-10 14:35:05 +0100

    milestone 4 task 5 modified, dummy added to store type

--- sql-files/milestone 4 task 5.sql
+++ sql-files/milestone 4 task 5.sql
@@ -1,4 +1,4 @@
-SELECT store_type, ROUND(SUM(p.product_price * o.product_quantity)::numeric, 2) AS total_sales, 
+SELECT s.store_type, ROUND(SUM(p.product_price * o.product_quantity)::numeric, 2) AS total_sales, 
 ROUND(COUNT(*) * 100/SUM(COUNT(*)) OVER ():: numeric, 2) AS percentage_total
 FROM orders_table AS o
 LEFT JOIN dim_store_details AS s
